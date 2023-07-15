96021c4722af01e87e0094e2aeee91551c542856
LuisDiegoSB2002 <ldsb20020611@gmail.com>
2023-07-15 01:07:54 -0600

    get, edit and delete implementados en el server (sin validacion de rol)

--- controlers/userController.js
+++ controlers/userController.js
@@ -1,5 +1,7 @@
 const User = require("../models/userModel");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secretKey = 'mysecretkey';
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -10,7 +12,7 @@ const register = async (req, res) => {
       const existingUser = await User.findOne({ email });
       console.log(existingUser);
       if (existingUser) {
-        return res.status(400).json({ error: 'El correo electrónico ya está en uso' });
+        return res.status(200).json({ error: 'El correo electrónico ya está en uso' });
       }
   
       // Crear un nuevo usuario con el rol "user"
@@ -30,20 +32,102 @@ const register = async (req, res) => {
       res.status(500).json({ error: 'Error al registrar el usuario' });
     }
   }
+  const login = async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      // Verificar si el usuario existe en la base de datos
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ error: 'Credenciales inválidas' });
+      }
+  
+      // Verificar si la contraseña es correcta
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return res.status(401).json({ error: 'Credenciales inválidas' });
+      }
+  
+      // Generar un token JWT con la información del usuario
+      const token = jwt.sign({ userId: user._id, role: user.role }, secretKey);
+  
+      res.status(200).json({ token });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al iniciar sesión' });
+    }
+  }
+
+  const checkAdminRole = (req, res, next) => {
+    if (req.user.role !== 'admin') {
+      return res.status(403).json({ error: 'Acceso no autorizado. Se requiere el rol de administrador.' });
+    }
+    next();
+  };
+
+  const edit = async (req, res) => {
+    console.log("hello");
+    const userId = req.params.id;
+    const { name, email, password } = req.body;
+  
+    try {
+      // Verificar si el usuario existe en la base de datos
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ error: 'El usuario no existe' });
+      }
+  
+      // Actualizar los datos del usuario
+      user.name = name;
+      user.email = email;
+      user.password = password;
+  
+      if (password) {
+        // Si se proporciona una nueva contraseña, encriptarla y guardarla
+        const hashedPassword = await bcrypt.hash(password, 10);
+        user.password = hashedPassword;
+      }
+  
+      await user.save();
+  
+      res.status(200).json({ message: 'Usuario actualizado correctamente' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al actualizar el usuario' });
+    }
+  }
+  const deleteUser = async (req, res) => {
+    const userId = req.params.id;
+  
+    try {
+      // Verificar si el usuario existe en la base de datos
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ error: 'El usuario no existe' });
+      }
+  
+      // Eliminar el usuario
+      await user.deleteOne();
+  
+      res.status(200).json({ message: 'Usuario eliminado correctamente' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al eliminar el usuario' });
+    }
+  }
+
+
   const obtener = async (req, res) => { 
     try {
-        const existingUser = await User.find();
-        if(existingUser){
-            res.status(200).json({error: "entra"});
-        }
-        else{
-            res.status(200).json({error: "NO "});
-        }
-    }
-    catch(e){
-        console.log(e.message);
+      // Obtener todos los usuarios de la base de datos
+      const users = await User.find({}, '-password'); // Excluir el campo de contraseña de la respuesta
+  
+      res.status(200).json(users);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al obtener los usuarios' });
     }
     
     
   }
-  module.exports = {register, obtener};
+  module.exports = {register, obtener, login,checkAdminRole,edit,deleteUser};
--- index.js
+++ index.js
@@ -2,13 +2,11 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+
 
 const app = express();
 const port = 3001;
-const secretKey = 'mysecretkey';
-const {register, obtener} = require("./controlers/userController");
+const {register, obtener, login, checkAdminRole, edit, deleteUser} = require("./controlers/userController");
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/proyecto', {
@@ -19,33 +17,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/proyecto', {
 app.use(express.json());
 
 app.post('/register', register);
-//app.get('/obtener', obtener);
+app.get('/obtener', obtener);
 
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+app.post('/login', login);
 
-  try {
-    // Verificar si el usuario existe en la base de datos
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(401).json({ error: 'Credenciales inválidas' });
-    }
+app.put('/edit/:id', edit );
 
-    // Verificar si la contraseña es correcta
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!passwordMatch) {
-      return res.status(401).json({ error: 'Credenciales inválidas' });
-    }
-
-    // Generar un token JWT con la información del usuario
-    const token = jwt.sign({ userId: user._id, role: user.role }, secretKey);
-
-    res.status(200).json({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error al iniciar sesión' });
-  }
-});
+app.delete('/deleteUser/:id', deleteUser );
 
 app.listen(port, () => {
   console.log(`Servidor iniciado en http://localhost:${port}`);
